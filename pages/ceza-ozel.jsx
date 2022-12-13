e93acd6ca1eebe112c057194dd35346f4f35c8ec
fatihtuzunn <fatihtuzun11@gmail.com>
2022-12-13 14:05:45 +0300

    new

--- pages/ceza-ozel.jsx
+++ pages/ceza-ozel.jsx
@@ -156,7 +156,7 @@ const CezaOzel = () => {
             isnadiyet: ""
 
         },
-        maneviUnsur: ".",
+        maneviUnsur: "",
         nitelikliUnsur: ""
         ,
         ozelGorunus: {
